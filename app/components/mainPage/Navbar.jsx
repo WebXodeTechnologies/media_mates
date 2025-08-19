bbebe207cb29cc83684f37079baf851c914c034a
ak220193 <mail2meak22frcrio@gmail.com>
2025-08-19 09:55:59 +0530

    Blog Added

--- app/components/mainPage/Navbar.jsx
+++ app/components/mainPage/Navbar.jsx
@@ -19,6 +19,7 @@ export default function Navbar() {
     { name: "Home", href: "/" },
     { name: "About Us", href: "/about" },
     { name: "Services", href: "/services" },
+    { name: "Blog", href: "/blog" },
     { name: "Contact Us", href: "/contact" },
   ];
 
